0f360ee76897fe58fb26ca40719d6151b028231f
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2017-10-12 12:34:31 +0200

    Check inputs

--- js/interpolator/events.js
+++ js/interpolator/events.js
@@ -1,12 +1,18 @@
 $(document).ready(function() {
 
     function interpolate() {
-        var x0 = $("#x0").val();
-        var x1 = $("#x1").val();
-        var y0 = $("#y0").val();
-        var y1 = $("#y1").val();
-        var x = $("#x").val();
-        $("#y").val(linearInterpolation(parseFloat(x), parseFloat(x0), parseFloat(x1), parseFloat(y0), parseFloat(y1)));
+        var x = parseFloat($("#x").val());
+        var x0 = parseFloat($("#x0").val());
+        var x1 = parseFloat($("#x1").val());
+        var y0 = parseFloat($("#y0").val());
+        var y1 = parseFloat($("#y1").val());
+
+        if (isNaN(x) || isNaN(x0) || isNaN(x1) || isNaN(y0) || isNaN(y1)) {
+            $("#alert-wrong-number").addClass('show');
+            setTimeout(function() { $("#alert-wrong-number").removeClass('show'); }, 3000);
+        } else {
+            $("#y").val(linearInterpolation(x, x0, x1, y0, y1));
+        }
     }
 
     $("#btn-submit").click(function() { interpolate() });
