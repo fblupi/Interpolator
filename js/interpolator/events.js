a397fac83c955f95d3f498ebd1e9f4da1cc74c63
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2017-10-12 12:15:56 +0200

    Basic interpolation

--- js/interpolator/events.js
+++ js/interpolator/events.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
 
+    function interpolate() {
+        var x0 = $("#x0").val();
+        var x1 = $("#x1").val();
+        var y0 = $("#y0").val();
+        var y1 = $("#y1").val();
+        var x = $("#x").val();
+        $("#y").val(linearInterpolation(parseFloat(x), parseFloat(x0), parseFloat(x1), parseFloat(y0), parseFloat(y1)));
+    }
+
+    $("#btn-submit").click(function() { interpolate() });
 });
